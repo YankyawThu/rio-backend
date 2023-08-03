cab3fccc41a52b3e93f0c5ccf3ea32adf4a8f9d4
Yankyaw Thu <yankyawthudev97@gmail.com>
2023-08-03 18:09:51 +0630

    fix image not showing

--- resources/views/admin/leagues/index.blade.php
+++ resources/views/admin/leagues/index.blade.php
@@ -54,7 +54,7 @@
                     <td>{{ $index + 1}}</td>
                     <td>
                       <div class="preview-thumbnail">
-                        <img src="{{ Storage::url($item->image) }}" alt="image" class="img-sm profile-pic"> 
+                        <img src="{{ asset(Storage::url($item->image)) }}" alt="image" class="img-sm profile-pic"> 
                       </div>
                     </td>
                     <td>{{ $item->name }}</td>
